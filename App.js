ca88a1b4e9361a335ffcd4baf72de30f22134208
Aydin <aydin.mirzagaev@gmx.de>
2024-05-05 13:46:43 +0200

    neue Struktur, Screens nach App.js verlagern

--- App.js
+++ App.js
@@ -1,36 +1,59 @@
 import * as React from 'react';
-import { createStaticNavigation } from '@react-navigation/native';
+import { Button, View, Text } from 'react-native';
+// import { View, Text, Button } from 'react-native-ui-lib';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './screens/Home';
-import Kategorie from './screens/Kategorie';
-import Bittgebete from './screens/Bittgebete';
 
-const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Hisnul Muslim',
-  screens: {
-    Home: {
-      screen: Home,
-      options: {
-        title: 'Hisnul Muslim',
-      },
-    },
-    Kategorie: {
-      screen: Kategorie,
-      options: {
-        title: 'Kategorie: XY',
-      },
-    },
-    Bittgebete: {
-      screen: Bittgebete,
-      options: {
-        title: 'Bittgebete',
-      },
-    },
-  },
-});
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Kategorien</Text>
 
-const Navigation = createStaticNavigation(RootStack);
-
-export default function App() {
-  return <Navigation />;
+      <View>
+        <Button
+          title="Go to Kategorie X"
+          onPress={() => navigation.navigate("Kategorie")}
+        />
+      </View>
+    </View>
+  );
 }
+
+function KategorieScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Kategorie X</Text>
+
+      <View>
+        <Button
+          title="Go to Bittgebet X"
+          onPress={() => navigation.navigate("Bittgebete")}
+        />
+      </View>
+    </View>
+  );
+}
+
+function BittgebeteScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Bittgebet X</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Hisnul Muslim" component={HomeScreen} />
+        <Stack.Screen name="Kategorie" component={KategorieScreen} />
+        <Stack.Screen name="Bittgebete" component={BittgebeteScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
